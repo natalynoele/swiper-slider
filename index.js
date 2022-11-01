c155517a3342af14bca14b18214c0ef450854367
natalynoele <natalynoele@gmail.com>
2022-11-01 12:15:54 +0200

    slider

---
+++ index.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.hero-slider', {
+  // Optional parameters
+  slidesPerView: "auto",
+            spaceBetween: 30,
+         centeredSlides: true,
+        spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
